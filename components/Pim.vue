be567e341618c9579cd1610450352de4c3ca43ae
BinoKing <bram.kuijpers1@gmail.com>
2024-05-08 10:16:11 +0200

    Pims Nuxt

---
+++ components/Pim.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Pim component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pim",
+  data() {
+    return {
+      // Your component's data goes here
+    };
+  },
+  computed: {
+    // Your computed properties go here
+  },
+  methods: {
+    // Your component's methods go here
+  },
+  mounted() {
+    // Code to run when the component is mounted goes here
+  },
+  // Other component options (e.g., watch, lifecycle hooks) go here
+};
+</script>
+
+<style scoped>
+/* Your component's styles go here */
+</style>
