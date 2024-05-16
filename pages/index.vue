77e984874c88fd920a8c07f3a9bc75a7b14add52
Pim <pim.kuijpers@gmail.com>
2024-05-16 20:16:57 +0200

    Added main.css and changes to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <h1>{{ pageTitle }}</h1>
-    <p>Welcome to my Nuxt.js homepage!</p>
-    <Pim />
-    <NuxtLink to="/about">About</NuxtLink>
-    <!-- This renders the Pim component -->
+  <div class="background">
+    <div class="container">
+      <h1 class="header-text">{{ pageTitle }}</h1>
+      <h1 class="header-text">Moon government</h1>
+      <span class="body-text">The elections are coming up. Refer to the voting guide to help you take your stand!</span>
+      <br>
+      <button class="button-main">button test</button>
+      <div class="box" style="height: 100px; width: 400px"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import Pim from "@/components/Pim.vue"; // Import the Pim component
+import "@/assests/css/main.css" // Import the main css 
 export default {
   components: {
     Pim, // Register the Pim component
@@ -22,8 +26,11 @@ export default {
 };
 </script>
 
-<style scoped>
-h1 {
-  color: #333;
+<style>
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
+
+
