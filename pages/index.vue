86fcdb9c052af4fce137b8dff1818ada6fcdd235
Pim <pim.kuijpers@gmail.com>
2024-05-22 14:14:02 +0200

    Changed index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="background">
-    <div class="container">
+    <!-- <div class="container"> -->
       <h1 class="header-text">{{ pageTitle }}</h1>
       <h1 class="header-text">Moon government</h1>
       <span class="body-text">The elections are coming up. Refer to the voting guide to help you take your stand!</span>
       <br>
       <button class="button-main">button test</button>
       <div class="box" style="height: 100px; width: 400px"></div>
-    </div>
+    <!-- </div> -->
   </div>
 </template>
 
