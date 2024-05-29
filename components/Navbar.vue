8f12f0fcc771be056a5abf2b4545253113aa8cdb
Rutten,Danny <i510404@fhict.nl>
2024-05-29 19:01:41 +0200

    Mobile navigation works now

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,25 +1,25 @@
 <template>
-  <header>
-    <div class="div-header-mobile">
-      <h1 class="h1-head">Title</h1>
-      <h2 class="h2-head" @click="showMobileMenu">[ MENU ]</h2>
-    </div>
-    <div class="div-header-desktop">
-      <h1 class="h1-head">Title</h1>
-      <h2 class="h2-head">Home</h2>
-      <h2 class="h2-head">Quests</h2>
-      <h2 class="h2-head">Profile</h2>
-      <h2 class="h2-head">Voting Guide</h2>
-    </div>
-  </header>
+	<header>
+		<div class="div-header-mobile">
+			<h1 class="h1-head">Title</h1>
+			<h2 class="h2-head" @click="showMobileMenu">[ MENU ]</h2>
+		</div>
+		<div class="div-header-desktop">
+			<h1 class="h1-head">Title</h1>
+			<h2 class="h2-head">Home</h2>
+			<h2 class="h2-head">Quests</h2>
+			<h2 class="h2-head">Profile</h2>
+			<h2 class="h2-head">Voting Guide</h2>
+		</div>
+	</header>
 </template>
 
 <script>
 export default {
-  methods: {
-    showMobileMenu() {
-      this.$emit("showMobileMenu");
-    },
-  },
+	methods: {
+		showMobileMenu() {
+			this.$emit("showMobileMenu");
+		},
+	},
 };
 </script>
